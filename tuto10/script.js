07d91968230f1c93690b47f224f3b27285330048
haliminds <hali.minds38@gmail.com>
2020-08-17 17:25:10 +0200

    Add tuto 10 on geoloc

---
+++ tuto10/script.js
@@ -0,0 +1,46 @@
+var map=null
+var marker = null
+
+function initialize() {
+	navigator.geolocation.getCurrentPosition(maPosition)
+	var survId = navigator.geolocation.watchPosition(surveillePosition, erreurPosition, {maximumAge:3000,enableHighAccuracy:false});
+}
+
+function erreurPosition(error) {
+    var info = "Erreur lors de la géolocalisation : ";
+    switch(error.code) {
+    case error.TIMEOUT:
+    	info += "Timeout !";
+    break;
+    case error.PERMISSION_DENIED:
+	info += "Vous n’avez pas donné la permission";
+    break;
+    case error.POSITION_UNAVAILABLE:
+    	info += "La position n’a pu être déterminée";
+    break;
+    case error.UNKNOWN_ERROR:
+    	info += "Erreur inconnue";
+    break;
+    }
+}
+
+
+function maPosition(position) {
+	map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+	var stamenToner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
+		attribution: 'Map tiles by Stamen Design, CC BY 3.0 — Map data © OpenStreetMap',
+		subdomains: 'abcd',
+		minZoom: 13,
+		maxZoom: 20,
+		ext: 'png'
+	});
+	// ajout du layer a la carte
+	map.addLayer(stamenToner);		
+	marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+}
+
+// Fonction de callback en cas de succès
+function surveillePosition(position) {
+	marker.setLatLng([position.coords.latitude, position.coords.longitude]);
+	map.panTo(position.coords.latitude, position.coords.longitude]);
+}
