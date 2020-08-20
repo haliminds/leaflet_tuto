e7742711524e8b9dd478772eddeb82c9190987e9
haliminds <hali.minds38@gmail.com>
2020-08-20 18:44:05 +0200

    Add tuto 11 with switchLayer plugin

---
+++ tuto11/leaflet-switchLayer_v2.js
@@ -0,0 +1,97 @@
+MIN_ZOOM = 0
+MAX_ZOOM = 25
+
+L.TileLayerZoomSwitch_v2 = L.TileLayer.extend({
+  includes: L.Evented,
+
+  options: {
+    // switchZoomStart: when zoom > switchZoomStart, use this layer
+    switchZoomStart: MIN_ZOOM,
+    // switchZoomEnd: when zoom <= switchZoomEnd, use this layer
+    switchZoomEnd: MAX_ZOOM,
+  },
+
+  getSwitchZoomStart: function() {
+    return this.options.switchZoomStart;
+  },
+
+  getSwitchZoomEnd: function() {
+    return this.options.switchZoomEnd;
+  }
+});
+
+L.tileLayerZoomSwitch_v2 = function(url, options) {
+	if (options.switchZoomStart > options.switchZoomEnd)
+	{
+  	throw new RangeError( "switchZoomStart cannot be superior to switchZoomEnd");
+	}
+	return new L.TileLayerZoomSwitch_v2(url, options);
+
+};
+
+
+
+
+/*
+ * SwitchLayerManager_v2 is a custom class for managing base layer automatic switching according to the current zoom level
+ */
+
+SwitchLayerManager_v2 = L.Class.extend({
+  _map: null,
+  options: {
+    baseLayers: null
+  },
+
+  initialize: function(map, options) {
+    this._map = map;
+    L.Util.setOptions(this, options);
+		// verify all zoom
+		this._verify_zoom()
+    // Update map for the firts time
+    this._update()
+    // update map for each zoomend
+    this._map.on({
+      'zoomend': this._update
+    }, this)
+  },
+
+  _update: function(e) {
+    const zoomCurrent = this._map.getZoom();
+    for (let i in this.options.baseLayers) {
+      const layer = this.options.baseLayers[i];
+      const zoomStart = layer.getSwitchZoomStart();
+      const zoomEnd = layer.getSwitchZoomEnd();
+
+      if ((zoomCurrent <= zoomEnd) && (zoomCurrent >= zoomStart)) {
+        layer.addTo(this._map)
+      } else {
+        this._map.removeLayer(layer)
+      }
+    }
+  },
+
+	_verify_zoom : function(e){
+		let zoom_end_max = 0
+		Object.entries(this.options.baseLayers).forEach((item, i) => {
+			zoom_end_max = Math.max(zoom_end_max, item[1].options.switchZoomEnd)
+		});
+		let test_zoom = Array.from({length: zoom_end_max+1}, (v, i) => 0)
+
+		// simplify test_zoom
+		for (let i in this.options.baseLayers) {
+			const layer = this.options.baseLayers[i];
+      const zoomStart = layer.getSwitchZoomStart();
+      const zoomEnd = layer.getSwitchZoomEnd();
+			for (z=zoomStart;z<=zoomEnd;z++)
+			{
+				test_zoom[z] += z;
+			}
+		}
+
+		// verifie si test_zoom est coherent ie test_zoom[z] = z;
+
+
+
+	}
+
+});
---
+++ tuto11/script.js
@@ -0,0 +1,24 @@
+function initialize() {
+  // center on Grenoble
+  map = L.map('map').setView([45.18, 5.740], 13);
+	const osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	const osmAttrib='Map data © openstreetmap contributors';
+	const cmUrl = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png';
+	const cmAttrib = 'Map tiles by Stamen Design, CC BY 3.0 — Map data © OpenStreetMap'
+	const opentopoUrl='https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+	const opentopoAttrib = 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+
+	cloudmade = new L.TileLayerZoomSwitch_v2(cmUrl, {attribution: cmAttrib, switchZoomEnd: 12} );                                      // 0 to 12
+	osm = new L.TileLayerZoomSwitch_v2(osmUrl,{minZoom:8, maxZoom:18, switchZoomStart: 13, switchZoomEnd: 13, attribution:osmAttrib}); // 13
+	testmade = new L.TileLayerZoomSwitch_v2(cmUrl, {maxZoom:15, attribution: cmAttrib, switchZoomStart: 14, switchZoomEnd: 15} );      // 14 to 15
+	opentopo = new L.TileLayerZoomSwitch_v2(opentopoUrl, {maxZoom:17, attribution: opentopoAttrib, switchZoomStart: 16} );             // 16 et +
+
+
+	const baseLayers = {
+	    "Cloudmade": cloudmade,
+	    "Osm": osm,
+			"testmade" : testmade,
+			"opentopo" : opentopo
+	};
+	switchManager = new SwitchLayerManager_v2(map, {baseLayers: baseLayers});
+}
